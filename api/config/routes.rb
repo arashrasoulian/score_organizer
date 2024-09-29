bbce08f1e73c8bb405c2361861de8ae5917d5d0e
Arash Rasoulian <arashrasoulian1@gmail.com>
2024-09-29 12:20:49 +0200

    first shape of home page was finished

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
         get 'statistics', to: 'statistics#index'
+        get 'scores/home_page_data', to: 'scores#home_page_data'
+
       end
     end
     resource :profile, only: [:show, :edit, :update]
