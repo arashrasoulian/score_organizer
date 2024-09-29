e534ea4c9ce69f12e026870cc5981f8fbd3dea22
Arash Rasoulian <arashrasoulian1@gmail.com>
2024-09-29 15:27:22 +0200

    first front of dashboard

--- api/app/controllers/api/v1/scores_controller.rb
+++ api/app/controllers/api/v1/scores_controller.rb
@@ -1,8 +1,6 @@
 module Api
   module V1
     class ScoresController < ApplicationController
-
-
       def home_page_data
         # 5 latest uploaded scores
         new_uploaded_scores = Score.order(created_at: :desc).limit(5)
@@ -13,20 +11,20 @@ module Api
         # 5 random scores for "For You"
         for_you_scores = Score.order("RANDOM()").limit(5)
 
-        instruments = ['viola', 'violin', 'cello', 'piano', 'vocal']
+        instruments = ["viola", "violin", "cello", "piano", "vocal"]
 
         # Building the response with additional random instrument for each score
         render json: {
           new_uploaded_scores: new_uploaded_scores.map { |score| format_score(score, instruments) },
           most_popular_scores: most_popular_scores.map { |score| format_score(score, instruments) },
-          for_you_scores: for_you_scores.map { |score| format_score(score, instruments) }
+          for_you_scores: for_you_scores.map { |score| format_score(score, instruments) },
         }
       end
+
       def index
         @scores = current_user.scores
         render json: @scores.map { |score| score_with_urls(score) }
       end
-
 
       def create
         @score = current_user.scores.build(score_params)
@@ -39,7 +37,6 @@ module Api
 
       private
 
-
       def score_params
         params.require(:score).permit(:score_pdf, :name, :composer, :score_type, :pdf)
       end
@@ -47,7 +44,7 @@ module Api
       def score_with_urls(score)
         score.as_json.merge(
           pdf_url: score.pdf.attached? ? rails_blob_url(score.pdf) : nil,
-          score_pdf: score.score_pdf
+          score_pdf: score.score_pdf,
         )
       end
 
@@ -55,7 +52,7 @@ module Api
         {
           name: score.name,
           composer: score.composer,
-          instrument: instruments.sample # Picks a random instrument
+          instrument: instruments.sample, # Picks a random instrument
         }
       end
     end
--- frontend/src/pages/dashboard/dashboardpages/Scores.js
+++ frontend/src/pages/dashboard/dashboardpages/Scores.js
@@ -1,31 +1,70 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Button } from "react-bootstrap";
 import { Myverticallycenteredmodal } from "../../../components/dashboardcomponents/Myverticallycenteredmodal";
-import ScoreList from "../../../components/Scorelist";
+import useFetch from "../../../hooks/useFetch";
+import Scorelist from "../../../components/homepage/Scorelist";
 
 const Scores = () => {
   const [modalShow, setModalShow] = React.useState(false);
+  const { data, loading, error } = useFetch(
+    "http://localhost:3000/api/v1/scores"
+  );
+const titles = ["Repertoire","Practicing","orcestral","for future"]
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   const currUser = useSelector((state) => state.user.currUser);
   if (!currUser) {
     return <p>Please log in to view your profile.</p>;
   }
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching data: {error.message}</p>;
 
   return (
-    <div>
+    <div className="myscores-container">
+      <div className="row  d-flex justify-content-center align-items-center searchbar-myscores-container">
+        <div className="col-md-8">
+          <div className="searchform-myscores">
+            <i className="fa fa-search"></i>
+            <input
+              type="text"
+              className="form-control searchform-myscores-input"
+              placeholder="Search anything..."
+            />
+            <span className="left-pan">
+              <i className="fa fa-microphone"></i>
+            </span>
+          </div>
+        </div>
+      </div>
 
-      <div className="col py-3">scores</div>
-      <Button variant="primary" onClick={() => setModalShow(true)}>
+      <Button
+        variant="primary"
+        onClick={() => setModalShow(true)}
+        className="m-4"
+      >
         add new pdf
       </Button>
 
-      <ScoreList />
+
+      <div className="row ">
+        {data && titles.map((title) => {
+          return (
+            <div key={title} className="col-4 col-md-3">
+              <Scorelist props={data} title={title} />
+            </div>
+          );
+        })}
+
+
+        </div>
       <Myverticallycenteredmodal
         show={modalShow}
         onHide={() => setModalShow(false)}
       />
-
     </div>
   );
 };
